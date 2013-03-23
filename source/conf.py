db52885d2bd927a3c7be34ebcc6d95698f5cf122
medmunds <medmunds@gmail.com>
2013-03-23 15:51:00 -0700

    Custom CSS styles

--- source/conf.py
+++ source/conf.py
@@ -96,6 +96,8 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'sphinxdoc'
 
+html_style = 'rockstar.css'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
